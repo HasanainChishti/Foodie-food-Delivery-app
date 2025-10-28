b9138dc6eb3d25ba2252236c122922e3865fe338
HasanainChishti <hasanainchisti2003@gmail.com>
2025-10-28 12:13:30 +0530

    css error solved

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-   plugins: [[react()],
-     
-    tailwindcss(),
-    
-],
- base: './',
-  // server: {
-  //   port: 3000, // change this to your desired port
-  //   host: '0.0.0.0', // or 'localhost' if you prefer
-  // },
- 
-  
-})
+  plugins: [react(), tailwindcss()],
+  base: "./",
+});
