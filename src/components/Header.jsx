afd0994308a85f9ac544d4b43debce4d17acd842
HasanainChishti <hasanainchisti2003@gmail.com>
2025-10-31 00:48:43 +0530

    update header compo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = ({scrollToContact}) => {
      <nav className="w-full h-20 flex items-center bg-orange-600 sticky top-0 shadow-md z-50 ">
          
         <div className="relative w-full max-w-7xl mx-auto px-4 sm:px-4 md:px-8  ">
-          <div className=" flex  items-center justify-between sm:flex sm:justify-around md:flex md:justify-evenly   lg:flex lg:justify-between">
+          <div className=" flex  items-center justify-between sm:flex sm:justify-around md:flex md:justify-evenly   lg:flex lg:justify-evenly">
             <div className="flex  items-center  p-2   rounded-xl ">
               {logo.map((char, i) => (
                 <span
@@ -51,8 +51,9 @@ const Header = ({scrollToContact}) => {
               </Link>
               
               <Link to={`/Search/${search || "Biryani"}`}>
-                <p className="text-xl font-semibold flex items-center gap-1 hover:text-black transition">
-                  <TfiSearch className="text-xl" />Search
+                <p className="text-xl font-semibold   py-1 flex items-center gap-1 hover:text-black transition">
+                  <TfiSearch className="text-xl" />
+                  search
                 </p>
               </Link>
               <button onClick={()=>{const section=document.getElementById('contact-section')
@@ -62,16 +63,16 @@ const Header = ({scrollToContact}) => {
                 </button>
 {/* https://cors-by-codethread-for-swiggy.vercel.app/cors/ */}
               <Link to="/CartPage">
-                <button className="flex items-center gap-1 text-xl font-semibold hover:text-black transition">
-                  <BsCart4 className="text-xl" /> 
-                  Cart{cnt?<span>({cnt.length})</span>:null}
+                <button className="flex relative items-center gap-1 text-xl font-semibold hover:text-black transition">
+                  <BsCart4 className="text-3xl " /> 
+                  {cnt!=0?<span className='absolute bottom-5 left-4  px-2 text-1/2 text-green-400  bg-white rounded-full'>{cnt.length}</span>:null}
                 </button>
               </Link>
 
               {!userData ? (
                 <Link to="/LogIn">
                   <button className="flex justify-center align-middle items-center gap-1 text-xl font-semibold hover:text-black transition px-3 py-1 rounded-lg">
-                    <MdLogin className="text-xl" /> Login
+                    {/* <MdLogin className="text-xl" /> Login */}
                   </button>
                 </Link>
               ) : (
