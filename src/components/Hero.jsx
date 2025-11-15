6d785ce439f874105d9ebc9f689806d4b904e7e3
HasanainChishti <hasanainchisti2003@gmail.com>
2025-11-15 10:39:22 +0530

    remove not working API features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,16 +18,16 @@ const Hero = ({scrollToMenu}) => {
   </div>
 
   {/* Content */}
-  <div className="absolute  flex flex-col top-5 ml-5  text-start text-white  h-120 sm:justify-center sm:align-middle sm:items-center ">
+  <div className="absolute  flex flex-col top-5 ml-5  text-start text-white   h-120 sm:justify-center sm:align-middle sm:items-center ">
     <h2 className="text-4xl  md:text-6xl font-extrabold mb-4 ">
       Delicious Food Delivered <br /> <span className="text-white">At Your Doorstep 🍽️</span>
     </h2>
     <p className="hidden md:flex text-lg md:text-3xl md:font-bold text-orange-400 mb-8 bg-white p-2 rounded-full">
       Discover your favorite dishes from top restaurants near you.
     </p>
-       <div className="location flex justify-center lg:hidden">
+       {/* <div className="location flex justify-center lg:hidden">
         <SearchBar></SearchBar>
-       </div>
+       </div> */}
     {/* Buttons */}   
     <div className=" hidden w-1/2 sm:flex justify-center mt-5 mx-auto sm:flex-row sm:w-full md:w-full  md:flex-row  gap-4">
       <button  onClick={scrollToMenu} className="bg-white hover:bg-amber-600 text-gray-800 font-semibold px-6 py-3 rounded-full shadow-md transition">
